242ceb64ddada7f3c0c2657ece4a1dd585e4b641
Nguyễn Minh Châu <chaunm27@fpt.com>
2025-08-16 17:50:26 +0700

    push code

--- vite.config.ts
+++ vite.config.ts
@@ -8,14 +8,4 @@ export default defineConfig({
 	build: {
 		outDir: 'dist', // Thư mục đầu ra mặc định của Vite
 	},
-	server: {
-		proxy: {
-			'/api': {
-				target: 'http://downloadtestspeed.vn',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, ''),
-			},
-		},
-		https: false, // Ensure Vite serves over HTTP
-	},
 });
