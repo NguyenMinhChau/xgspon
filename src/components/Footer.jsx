c796488065e8efa9aea2df2df1053f9acd38abfe
Nguyễn Minh Châu <chaunm27@fpt.com>
2025-08-16 14:12:00 +0700

    push code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,9 @@ export default function Footer() {
 		<React.Fragment>
 			<div
 				className="border-t border-white py-2 px-1 lg:!bg-transparent"
-				style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
+				style={{ backgroundColor: 'rgba(0, 0, 0, 0.1)' }}
 			>
-				<div
-					className="w-full lg:w-[60%] md:w-[80%] text-[15px] h-auto text-white"
-					style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}
-				>
+				<div className="w-full lg:w-[60%] md:w-[80%] text-[15px] h-auto text-white">
 					Website được sử dụng duy nhất với mục đích test/trải nghiệm tốc độ
 					download dữ liệu dung lượng lớn trong sự kiện ra mắt công nghệ XGS-PON
 					và Wi-Fi 7 của FPT
